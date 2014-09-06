2faca4cd3a8ca83abc67c0b223f6bd4a4441687b
Yorrick Jansen <yorrick.jansen@gmail.com>
2014-09-06 17:43:14 -0400

    tests

--- django_email_sender/ems_messages/tests.py
+++ django_email_sender/ems_messages/tests.py
@@ -38,6 +38,29 @@ class ViewTest(TestCase):
             self.assertContains(response, "Bienvenue")
             self.assertEqual(len(response.context['message_list']), 2)
 
+    def test_create_new_message(self):
+        data = {
+            'user': 1,
+            'source': 'the source',
+            'destination': 'the destination',
+            'content': 'the content',
+        }
+
+        response = self.client.post(reverse('message:index'), data)
+        # redirect means success
+        self.assertEqual(response.status_code, 302)
+
+    def test_form_validation(self):
+        data = {
+            'user': 1,
+            'source': 'the source',
+            'destination': 'the destination',
+            'content': 'toto content',
+            }
+
+        response = self.client.post(reverse('message:index'), data)
+        # 200 means error
+        self.assertEqual(response.status_code, 200)
 
 
 @contextlib.contextmanager
