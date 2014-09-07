2b9a30205ddd2b564476b63015ce9732d7fe35b2
Yorrick Jansen <yorrick.jansen@gmail.com>
2014-09-07 17:44:28 -0400

    prod settings

--- django_email_sender/settings_prod.py
+++ django_email_sender/settings_prod.py
@@ -1,21 +1,9 @@
 # Production settings
-
 from django_email_sender.settings_common import *
-
 import dj_database_url
 
-DATABASES['default'] =  dj_database_url.config()
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'docker',
-        'USER': 'docker',
-        'PASSWORD': 'docker',
-        'HOST': '192.168.59.103',  # host of docker VM
-        'PORT': '5432',
-        },
-}
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
